db9ffc1dac8226d0fd877f097179d67ec9768626
MohamadAlGhorani <moh.gh266@gmail.com>
2019-10-27 15:55:29 +0100

    adding class name for hint is now working in css

--- my-app/src/app/hint/hint.component.ts
+++ my-app/src/app/hint/hint.component.ts
@@ -5,7 +5,7 @@ import { Component, OnInit, Input } from "@angular/core";
   templateUrl: "./hint.component.html",
   styleUrls: ["./hint.component.scss"],
   template: `
-    <p class="{{ hintClassName }}">{{ title }}</p>
+    <p class=" hint {{ hintClassName }}">{{ title }}</p>
   `
 })
 export class HintComponent implements OnInit {
