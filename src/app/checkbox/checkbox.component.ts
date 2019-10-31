aee78de1205587288f858788137901c17a53c628
MohamadAlGhorani <moh.gh266@gmail.com>
2019-10-31 16:27:13 +0100

    test

--- src/app/checkbox/checkbox.component.ts
+++ src/app/checkbox/checkbox.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, Output, EventEmitter } from "@angular/core";
 
 @Component({
   selector: "app-checkbox",
-  templateUrl: "./checkbox.component.html",
   styleUrls: ["./checkbox.component.scss"],
   template: `
     <form action="">
